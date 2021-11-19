617e15b1bab49ba7082da84500103c7a21a568e2
OshanYelena <oshanranasinghe25@gmail.com>
2021-11-19 13:42:22 +0530

    app

---
+++ src/components/loading.js
@@ -0,0 +1,10 @@
+import '../css/loading.css'
+
+const Loading = () => {
+    return (
+        <div className="grad-div" style={{ backgroundImage: `url(/hackdoze01/hack-doze/cover_web_1.png)` }}></div>
+    )
+}
+
+
+export default Loading
